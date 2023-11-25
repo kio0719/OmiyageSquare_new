248afaa6011b0be016d514103a1eeb386f57d635
ioioio0719 <kio0719@docomo.ne.jp>
2023-11-25 14:33:59 +0900

    add: 現在のユーザーの情報を表示する機能を追加

--- frontend/src/views/contents/Top.vue
+++ frontend/src/views/contents/Top.vue
@@ -1,14 +1,44 @@
 <template>
   <div>
     <h1>Top</h1>
-    <button type="button" @click="signOut">ログアウト</button>
+    <div v-if="currentUser">
+      <p>ようこそ!{{ currentUser.email }}さん！</p>
+      <button type="button" @click="signOut">ログアウト</button>
+    </div>
+    <div v-else>
+      <p>ログインしてください</p>
+      <a href="/">ログイン画面へ</a>
+    </div>
   </div>
 </template>
 <script setup>
 import axios from "axios";
+import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+const currentUser = ref();
+
+onMounted(() => {
+  getCurrentUser()
+})
+
+const getCurrentUser = () => {
+  axios
+    .get('/current_user',{
+      headers: {
+        uid: localStorage.getItem("uid"),
+        'access-token': localStorage.getItem("access-token"),
+        client: localStorage.getItem("client")
+      }
+    })
+    .then(response => {
+      currentUser.value = response.data;
+    })
+    .catch(error => {
+      console.error(error);
+    })
+}
 
 const signOut = () => {
   axios
