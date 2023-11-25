8f9f41dbb1e7dff0fd7be0dc498312f7d0b34a05
ioioio0719 <kio0719@docomo.ne.jp>
2023-11-25 20:20:04 +0900

    add: ユーザー登録機能作成

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../views/authentications/Login.vue';
 import Top from '../views/contents/Top.vue';
+import SignUp from '../views/authentications/SignUp.vue';
 
 const routes = [
   { path: '/', name: "login", component: Login },
-  { path: '/top', name: "top", component: Top }
+  { path: '/top', name: "top", component: Top },
+  { path: '/signUp', name: "signUp",component: SignUp }
 ]
 
 const router = createRouter({
